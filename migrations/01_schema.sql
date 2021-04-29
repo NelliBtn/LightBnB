cfdc8a75b445abbaa65ab3853b7a7e72ac533a14
Nelli Butenko <butenkonelly93@gmail.com>
2021-04-29 00:10:45 +0000

    Fix typos

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -16,12 +16,12 @@ CREATE TABLE properties (
 
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  cost_per_night SMALLINT NOT NULL DEFAULT 0,
-  parking_spaces SMALLINT NOT NULL DEFAULT 0,
-  number_of_bathrooms SMALLINT NOT NULL DEFAULT 0,
-  number_of_bedrooms SMALLINT NOT NULL DEFAULT 0,
-  thumbnail_photo VARCHAR(255) NOT NULL,
-  cover_photo VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  parking_spaces INTEGER NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
 
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
@@ -29,22 +29,22 @@ CREATE TABLE properties (
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
   
-  is_active BOOLEAN NOT NULL DEFAULT TRUE
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  guest_id INT REFERENCES users(id) ON DELETE CASCADE,
-  property_id INT REFERENCES properties(id) ON DELETE CASCADE
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INT REFERENCES users(id) ON DELETE CASCADE,
-  property_id INT REFERENCES properties(id) ON DELETE CASCADE,
-  reservation_id  INT REFERENCES reservations(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   
   message TEXT,
   rating SMALLINT NOT NULL DEFAULT 0
